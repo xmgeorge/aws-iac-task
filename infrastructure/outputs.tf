b1bdce589187e852ac2cf42ebc1f120f26381bdc
George Ulahannan <george.ulahannan@gmail.com>
2026-07-26 21:15:31 +0100

    Expose VPC id and public subnet ids as root outputs

---
+++ infrastructure/outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnet_ids
+}
